2e62202f6919a8120bd6a561ec0eca15dd601c8a
nazmul <marzu322@outlook.com>
2025-02-10 19:26:34 +0600

    js

---
+++ module17/partice.js
@@ -0,0 +1,57 @@
+// let price = 400;
+
+// if(price >= 600 ) {
+//     console.log('free coke');
+
+// }
+// else{
+//     console.log('coke 30tk');
+// }
+
+
+// bmi
+
+
+let weight = 70;
+let  height = 1.75;
+let bmi = weight / (height * height)
+
+if (bmi < 18.5 ){
+    console.log('underweight')
+}
+else if(bmi >= 18.5 && bmi <=24.9){
+    console.log('normal')
+}
+else if(bmi >=25 && bmi <= 29.9){
+    console.log('overweight')
+}
+
+
+else{
+    console.log('you are obse')
+}
+
+
+
+
+
+
+//  students marks 
+// let marks  = 59;
+ 
+// if ( marks >= 80 && marks == 100){
+//     console.log('pola A+ paice')
+// }
+
+// else if (marks >= 70 && marks == 79){
+//     console.log('motamut parce')
+// }
+// else if (marks >= 60 && marks == 69 ){
+//     console.log('chole r ki')
+// }
+// else if (marks >= 0 && marks == 59){
+//     console.log('fail korce')
+// }
+// else{
+//     console.log('pola dabba marce')
+// }
