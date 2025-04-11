db5764917d96cf3b69332e403a479f69cdc5c789
nazmul <marzu322@outlook.com>
2025-04-11 23:12:55 +0600

    da

--- second-react/src/App.jsx
+++ second-react/src/App.jsx
@@ -1,8 +1,17 @@
 import Counter from './counter';
 import './App.css'
 import Batsman from './batsman';
+import Users from './user';
+import Friends from './friends';
+import { Suspense } from 'react';
+
+const fetchfriends = async ()=>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+ return res.json()
+}
 
 function App() {
+  const friendsPRomise = fetchfriends();
   function handleClick(){
     alert('i am clicked')
   }
@@ -12,6 +21,14 @@ function App() {
   }
   return (
     <>
+
+    <Suspense fallback={<h3>Friends are comings....</h3>}>
+        <Friends friendspromis={friendsPRomise} ></Friends>
+    </Suspense>
+     
+
+
+    <Users></Users>
     <Batsman></Batsman>
       
       <h1>Vite + React</h1>
