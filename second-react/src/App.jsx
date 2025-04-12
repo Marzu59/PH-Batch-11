5f825683a0feddfa26658a90cc4086c2b5daef15
nazmul <marzu322@outlook.com>
2025-04-12 07:40:00 +0600

    da

--- second-react/src/App.jsx
+++ second-react/src/App.jsx
@@ -4,14 +4,24 @@ import Batsman from './batsman';
 import Users from './user';
 import Friends from './friends';
 import { Suspense } from 'react';
+import Posts from './posts';
 
-const fetchfriends = async ()=>{
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
- return res.json()
+// const fetchfriends = async ()=>{
+//   const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//  return res.json()
+// }
+
+const fetchPosts =async ()=>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  return res.json();
 }
 
+
+
 function App() {
-  const friendsPRomise = fetchfriends();
+  const promisePosts = fetchPosts()
+  // const friendsPRomise = fetchfriends();
+
   function handleClick(){
     alert('i am clicked')
   }
@@ -21,10 +31,14 @@ function App() {
   }
   return (
     <>
+         <Suspense fallback={<h4>Posts are loding..</h4>}>
+            <Posts postsPromise={promisePosts} ></Posts>
+         </Suspense>
 
-    <Suspense fallback={<h3>Friends are comings....</h3>}>
+
+    {/* <Suspense fallback={<h3>Friends are comings....</h3>}>
         <Friends friendspromis={friendsPRomise} ></Friends>
-    </Suspense>
+    </Suspense> */}
      
 
 
